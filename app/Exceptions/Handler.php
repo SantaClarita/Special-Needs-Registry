db192787a0c32c773f3e1998f91c5bf5e8555501
chernandezSC <chernandez@santa-clarita.com>
2019-07-17 17:27:36 -0700

    Token Message Expired Message
    Other small JS fixes and SBC/AT&T message

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -45,6 +45,12 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $e)
     {
+        if($e instanceof \Illuminate\Session\TokenMismatchException){
+            return redirect()
+                ->back()
+                ->withInput($request->except('_token'))
+                ->withMessage('Your session has expired!');
+        }
         return parent::render($request, $e);
     }
 }
